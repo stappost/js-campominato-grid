68e3ab65cd562f891050709c0cd672b78d8c3768
stappost <emanuele.scarpantoni@gmail.com>
2023-11-29 17:29:55 +0100

    fix button

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ const grid = document.getElementById("grid");
 const button = document.getElementById("play");
 
 button.addEventListener("click", function(){
+    grid.innerHTML = " ";
     for(i=1; i<=100; i++){
         let square = create_cell(i);
         grid.appendChild(square);
