68619983d55e6ae2816db8141de37ad999b642bb
stappost <emanuele.scarpantoni@gmail.com>
2023-11-29 16:21:42 +0100

    add bonus folder

---
+++ bonus/js/script.js
@@ -0,0 +1,19 @@
+// FUNZIONE CHE CREA CELLE 
+function create_cell(num){
+    let element = document.createElement("div");
+    element.classList.add("square");
+    element.innerText = num;
+
+    return element;
+}
+
+// RECUPERO BUTTON E DIV #GRID 
+const grid = document.getElementById("grid");
+const button = document.getElementById("play");
+
+button.addEventListener("click", function(){
+    for(i=1; i<=100; i++){
+        let square = create_cell(i);
+        grid.appendChild(square);
+    }
+})
