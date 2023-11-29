47af4db12d8767bd5bbd293892fc15f1e802b846
stappost <emanuele.scarpantoni@gmail.com>
2023-11-29 16:32:43 +0100

    finish first bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -15,5 +15,9 @@ button.addEventListener("click", function(){
     for(i=1; i<=100; i++){
         let square = create_cell(i);
         grid.appendChild(square);
+        square.addEventListener("click", function(){
+            this.classList.toggle("bg_lightblue");
+            console.log(square.innerText)
+        })
     }
 })
